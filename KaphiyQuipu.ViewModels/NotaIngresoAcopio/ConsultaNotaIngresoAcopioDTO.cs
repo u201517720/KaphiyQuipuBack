d2ab8f82dede291bc5241a83b5ce8be236306f28
Jordan Dávila Terrones <u201517720@upc.edu.pe>
2021-11-08 01:57:47 -0500

    ingreso almacen

---
+++ KaphiyQuipu.ViewModels/NotaIngresoAcopio/ConsultaNotaIngresoAcopioDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaphiyQuipu.DTO
+{
+    public class ConsultaNotaIngresoAcopioDTO
+    {
+        public int NotaIngresoAcopioId { get; set; }
+        public int GuiaRecepcionId { get; set; }
+        public string Correlativo { get; set; }
+        public string CorrelativoGR { get; set; }
+        public string RazonSocial { get; set; }
+        public int TotalSacos { get; set; }
+        public decimal PesoSaco { get; set; }
+        public string Calidad { get; set; }
+        public string Certificacion { get; set; }
+        public string HashBC { get; set; }
+        public string UsuarioRegistro { get; set; }
+        public string FechaRegistro { get; set; }
+        public string Estado { get; set; }
+        public string EstadoId { get; set; }
+    }
+}
---
+++ KaphiyQuipu.ViewModels/NotaIngresoAcopio/ConsultaNotaIngresoAcopioResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using Core.Common.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaphiyQuipu.DTO
+{
+    public class ConsultaNotaIngresoAcopioResponseDTO
+    {
+        public ConsultaNotaIngresoAcopioResponseDTO()
+        {
+            Result = new Result();
+        }
+
+        public Result Result { get; set; }
+    }
+}
